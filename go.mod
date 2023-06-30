6dc380ba3b6894ca55a84fd6a177b55cbd4c3720
sonyamoonglade <aalexandrovich.hireme@gmail.com>
2023-06-30 19:06:07 +0400

    initial commit

---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/sonyamoonglade/go_translit
+
+go 1.20
+
+require (
+	github.com/sonyamoonglade/go_func v0.0.0-20230430081942-b7c96d7b6919
+	github.com/stretchr/testify v1.7.1
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/exp v0.0.0-20220321173239-a90fa8a75705 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
+)
---
+++ translate.go
@@ -0,0 +1,322 @@
+package go_translit
+
+import (
+	"fmt"
+	"strings"
+	"unicode"
+	"unicode/utf8"
+	"unsafe"
+)
+
+import (
+	fn "github.com/sonyamoonglade/go_func"
+)
+
+var dict = map[string]string{
+	"А": "A",
+	"а": "a",
+
+	"Б": "B",
+	"б": "b",
+
+	"В": "V",
+	"в": "v",
+
+	"Г": "G",
+	"г": "g",
+
+	"Д": "D",
+	"д": "d",
+
+	"Е": "E",
+	"е": "e",
+
+	"Ё": ">",
+	"ё": "<",
+
+	"Ж": "J",
+	"ж": "j",
+
+	"З": "Z",
+	"з": "z",
+
+	"И": "I",
+	"и": "i",
+
+	"Й": "~",
+	"й": "*",
+
+	"К": "K",
+	"к": "k",
+
+	"Л": "L",
+	"л": "l",
+
+	"М": "M",
+	"м": "m",
+
+	"Н": "N",
+	"н": "n",
+
+	"О": "O",
+	"о": "o",
+
+	"П": "P",
+	"п": "p",
+
+	"Р": "R",
+	"р": "r",
+
+	"С": "S",
+	"с": "s",
+
+	"Т": "T",
+	"т": "t",
+
+	"У": "U",
+	"у": "u",
+
+	"Ф": "F",
+	"ф": "f",
+
+	"Х": "H",
+	"х": "h",
+
+	"Ч": ")",
+	"ч": "(",
+
+	"Ш": "&",
+	"ш": "?",
+
+	"Щ": "$",
+	"щ": "%",
+
+	"ц": "#",
+	"Ц": "!",
+
+	"ъ": "@",
+	"ы": "+",
+
+	"Ю": "]",
+	"ю": "[",
+
+	"Я": "}",
+	"я": "{",
+
+	"ь": ".",
+
+	"э": ",",
+}
+
+var reversedDict = map[string]string{
+	"A": "А",
+	"a": "а",
+
+	"B": "Б",
+	"b": "б",
+
+	"V": "В",
+	"v": "в",
+
+	"G": "Г",
+	"g": "г",
+
+	"D": "Д",
+	"d": "д",
+
+	"E": "Е",
+	"e": "е",
+
+	">": "Ё",
+	"<": "ё",
+
+	"J": "Ж",
+	"j": "ж",
+
+	"Z": "З",
+	"z": "з",
+
+	"I": "И",
+	"i": "и",
+
+	"~": "Й",
+	"*": "й",
+
+	"K": "К",
+	"k": "к",
+
+	"L": "Л",
+	"l": "л",
+
+	"M": "М",
+	"m": "м",
+
+	"N": "Н",
+	"n": "н",
+
+	"O": "О",
+	"o": "о",
+
+	"P": "П",
+	"p": "п",
+
+	"R": "Р",
+	"r": "р",
+
+	"S": "С",
+	"s": "с",
+
+	"T": "Т",
+	"t": "т",
+
+	"U": "У",
+	"u": "у",
+
+	"F": "Ф",
+	"f": "ф",
+
+	"H": "Х",
+	"h": "х",
+
+	")": "Ч",
+	"(": "ч",
+
+	"&": "Ш",
+	"?": "ш",
+
+	"$": "Щ",
+	"%": "щ",
+
+	"#": "ц",
+	"!": "Ц",
+
+	"@": "ъ",
+
+	"+": "ы",
+
+	"]": "Ю",
+	"[": "ю",
+
+	"}": "Я",
+	"{": "я",
+
+	".": "ь",
+
+	",": "э",
+}
+
+const (
+	noEncPrefixByte = '='
+	noEncPrefixStr  = "="
+	spaceSeparator  = " "
+)
+
+func Encode(values []string) []string {
+	return fn.
+		Of(values).
+		Map(func(value string, _ int) string {
+			return fn.
+				OfSplitString(value, spaceSeparator).
+				Map(func(word string, _ int) string {
+					return encodeWord(word)
+				}).
+				Join(spaceSeparator)
+		}).
+		Result
+}
+
+func Decode(encoded []string) []string {
+	return fn.
+		Of(encoded).
+		Map(func(value string, _ int) string {
+			return fn.
+				OfSplitString(value, spaceSeparator).
+				Map(func(word string, _ int) string {
+					return decodeWord(word)
+				}).
+				Join(spaceSeparator)
+		}).
+		Result
+}
+
+func EncodeWord(word string) string {
+	if strings.Contains(word, " ") {
+		return strings.Join(Encode(strings.Split(word, spaceSeparator)), " ")
+	}
+	return encodeWord(word)
+}
+
+func DecodeWord(word string) string {
+	if strings.Contains(word, " ") {
+		return strings.Join(Decode(strings.Split(word, spaceSeparator)), " ")
+	}
+	return decodeWord(word)
+}
+
+func encodeWord(word string) string {
+	if isNumerical(word) {
+		fmt.Println("here 3")
+		return word
+	}
+	if isEnglishOrNumericalOrSpecial(word) {
+		fmt.Println("here 2")
+		return noEncPrefixStr + word
+	}
+	fmt.Println("here 3")
+	return fn.Compose2(byteSliceToStr, encodeToAscii)(word)
+}
+
+func decodeWord(word string) string {
+	if isNumerical(word) {
+		return word
+	}
+	if isNotEncoded(word) {
+		return word[len(noEncPrefixStr):]
+	}
+	return fn.Compose2(byteSliceToStr, decodeFromAscii)(word)
+}
+
+func isEnglishOrNumericalOrSpecial(word string) bool {
+	for _, ch := range word {
+		if !(('a' <= ch && ch <= 'z') || ('A' <= ch && ch <= 'Z') || unicode.IsDigit(ch) || (unicode.IsPunct(ch))) {
+			return false
+		}
+	}
+	return true
+}
+
+func isNumerical(word string) bool {
+	for _, ch := range word {
+		if !unicode.IsDigit(ch) {
+			return false
+		}
+	}
+	fmt.Println("here")
+	return true
+}
+
+func encodeToAscii(s string) []byte {
+	return fn.Reduce(func(acc []byte, ch rune, _ int) []byte {
+		if encoded, ok := dict[string(ch)]; ok {
+			return append(acc, []byte(encoded)...)
+		}
+		return utf8.AppendRune(acc, ch)
+	}, []rune(s), make([]byte, 0, len(s)))
+}
+
+func decodeFromAscii(s string) []byte {
+	return fn.Reduce(func(acc []byte, ch rune, _ int) []byte {
+		if decoded, ok := reversedDict[string(ch)]; ok {
+			return append(acc, []byte(decoded)...)
+		}
+		return utf8.AppendRune(acc, ch)
+	}, []rune(s), make([]byte, 0, len(s)))
+}
+
+func byteSliceToStr(b []byte) string {
+	return *(*string)(unsafe.Pointer(&b))
+}
+
+func isNotEncoded(s string) bool {
+	return s[0] == noEncPrefixByte
+}
